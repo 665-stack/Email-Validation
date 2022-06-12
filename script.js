36d749a3c19f8f1a9bd4906166df459e74f5d97f
unknown <mahmodulshahin665@gmail.com>
2022-06-12 13:30:48 +0600

    add regex and filter email and alert massage

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
-
+const validEmailAlert = document.getElementById('valid');
+const invalidEmailAlert = document.getElementById('invalid');
 
 const validate = () => {
-    const inputMail = document.getElementById('email').value;
+    const inputMail = document.getElementById('text').value;
+    const regx = /^([a-zA-Z0-9\._]+)@([a-zA-Z0-9])+.([a-z]+)(.[a-z]+)?$/
 
+    if (regx.test(inputMail)) {
+        invalidEmailAlert.style.display = 'none';
+        validEmailAlert.style.display = 'block';
+        return true;
+
+    }
+    else {
+        validEmailAlert.style.display = 'none';
+        invalidEmailAlert.style.display = 'block';
+        return false;
+    }
+
+    // clear input field
+    document.getElementById('email').value = "";
 
 }
